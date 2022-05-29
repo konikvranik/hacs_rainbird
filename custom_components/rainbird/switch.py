02848cbae12ef74e789e55376166964a773d4eb9
pvranik <petr.vranik@mgm-tp.com>
2022-05-29 10:01:45 +0200

    start_zone service

--- custom_components/rainbird/switch.py
+++ custom_components/rainbird/switch.py
@@ -1,5 +1,6 @@
 """Support for Rain Bird Irrigation system LNK WiFi Module."""
 import logging
+from typing import Any, Coroutine
 
 import asyncio
 import voluptuous as vol
@@ -10,11 +11,15 @@ from homeassistant.const import (
     CONF_SWITCHES,
     CONF_TRIGGER_TIME,
     CONF_ZONE, CONF_HOST, )
-from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import config_validation as cv, entity_platform
 from homeassistant.helpers.typing import HomeAssistantType
 from pyrainbird import RainbirdController
+from homeassistant.exceptions import HomeAssistantError
 
 from . import RuntimeEntryData, DOMAIN, RainbirdEntity
+
+CONF_ZONE_RUN_TIME = "zone_run_time"
+DEFAULT_ZONE_RUN = 120
 
 _LOGGER = logging.getLogger(__name__)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -39,6 +44,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         async_add_entities(future.result(), True)
 
     hass.async_add_executor_job(_get_entities, config_entry, data, hass).add_done_callback(_add_entities)
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service("start_zone", {
+        vol.Optional(
+            CONF_ZONE_RUN_TIME, default=DEFAULT_ZONE_RUN
+        ): cv.positive_int
+    }, "async_start_zone")
 
 
 def _get_entities(config_entry, data: RuntimeEntryData, hass: HomeAssistantType):
@@ -67,11 +78,11 @@ class RainBirdSwitch(RainbirdEntity, SwitchEntity):
                  data: RuntimeEntryData = None):
         """Initialize a Rain Bird Switch Device."""
         self._zone = int(device_info.get(CONF_ZONE))
-        self._duration = device_info.get(CONF_TRIGGER_TIME)
+        self._attr_duration = device_info.get(CONF_TRIGGER_TIME)
         super(RainBirdSwitch, self).__init__(hass, rb, device_info.get("id"),
                                              device_info.get(CONF_FRIENDLY_NAME, "Rainbird {} #{}").format(
                                                  device_info.get(CONF_HOST), self._zone), data, 'mdi:sprinkler-variant',
-                                             attributes={"duration": self._duration, "zone": self._zone})
+                                             attributes={"duration": self._attr_duration, "zone": self._zone})
         self._state = None
 
     @property
@@ -81,7 +92,7 @@ class RainBirdSwitch(RainbirdEntity, SwitchEntity):
 
     def turn_on(self, **kwargs):
         """Turn the switch on."""
-        duration = kwargs["duration"] if "duration" in kwargs else self._duration
+        duration = kwargs["zone_run_time"] if "zone_run_time" in kwargs else self._attr_duration
         response = self._controller.irrigate_zone(int(self._zone), int(duration // 60))
         if response:
             self._state = True
@@ -100,3 +111,7 @@ class RainBirdSwitch(RainbirdEntity, SwitchEntity):
     def update(self):
         """Update switch status."""
         self._state = self._controller.get_zone_state(self._zone)
+
+    async def async_start_zone(self, *, zone_run_time: int) -> None:
+        """Start a particular zone for a certain amount of time."""
+        await self.async_turn_on(duration=zone_run_time)
